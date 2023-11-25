c55a98027c52677887b4a53f03a23d1f42e4f491
Devyaansh Dwivedi <91078095+devyaanshdwivedi@users.noreply.github.com>
2023-11-25 23:42:53 +0530

    Add files via upload

---
+++ Stack using two queues.py
@@ -0,0 +1,71 @@
+#User function Template for python3
+
+
+def push(x):
+    
+   
+    global queue_1
+    global queue_2
+    
+    
+ 
+    queue_1.append(x)
+
+ 
+
+def pop():
+    
+   
+    global queue_1
+    global queue_2
+   
+    
+  
+    if queue_1:
+       return queue_1.pop()
+    else:
+        return -1
+    
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+queue_1 = [] # first queue
+queue_2 = [] # second queue
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        a = list(map(int,input().strip().split()))
+        i = 0
+        while i<len(a):
+            if a[i] == 1:
+                push(a[i+1])
+                i+=1
+            else:
+                print(pop(),end=" ")
+            i+=1
+
+        # clear both the queues
+        queue_1 = []
+        queue_2 = []
+        print()
+# } Driver Code Ends
